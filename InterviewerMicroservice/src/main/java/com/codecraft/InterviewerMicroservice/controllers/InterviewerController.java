6936bd3f7f73dbad91492bbee9d6cb1db1b89c0f
Abhishek Gupta <abhig0209@gmail.com>
2024-05-25 10:35:04 +0530

    logs added for interviewer controller

--- InterviewerMicroservice/src/main/java/com/codecraft/InterviewerMicroservice/controllers/InterviewerController.java
+++ InterviewerMicroservice/src/main/java/com/codecraft/InterviewerMicroservice/controllers/InterviewerController.java
@@ -24,6 +24,7 @@ public class InterviewerController {
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequest) {
+        logger.info("Login Request DTO Recieved: " + loginRequest);
         String token = interviewerService.login(loginRequest.getEmail(), loginRequest.getPassword());
         if (token != null) {
             return ResponseEntity.ok(token);
@@ -33,12 +34,14 @@ public class InterviewerController {
     }
     @GetMapping("/activeJobs/{id}")
     public int activeJobsCountController(@PathVariable int id){
+        logger.info("Request to find whether a job is active or not received: " + id);
         return interviewerService.activeJobsCount(id);
 
     }
     //for candidate fiegn client
     @GetMapping("/all-active-jobs")
     public List<Job> allactiveJobsCountController(){
+        logger.info("Request for all active job count received");
         List<Job> jobs =interviewerService.allactiveJobsCount();
         logger.info("Info log message");
         return jobs;
@@ -47,28 +50,33 @@ public class InterviewerController {
 
     @GetMapping("/closed-jobs/{id}")
     public List<JobInfoDTO> closedJobController(@PathVariable int id){
+        logger.info("Request to close job received: " + id);
         return interviewerService.getClosedJobs(id);
 
     }
 
     @PostMapping("/scheduleInterview")
     public void scheduleInterview(@RequestBody ScheduleInterviewDTO ScheduleInterviewDTO){
-        System.out.println(true);
+        logger.info("Schedule Interview Request DTO Recieved: " + ScheduleInterviewDTO);
+//        System.out.println(true);
         System.out.println(ScheduleInterviewDTO.getInterviewDate());
         interviewerService.scheduleInterview(ScheduleInterviewDTO);
     }
 @PostMapping("/result")
     public void interviewResults(@RequestBody InterviewRecordInfoDTO dto){
+    logger.info("Interview Record Info DTO Recieved: " + dto);
     System.out.println(dto);
     interviewerService.interviewResult(dto);
     }
     @GetMapping("/count/{id}")
     public CountDTO counter(@PathVariable int id){
+        logger.info("Count DTO Recieved: " + id);
         return interviewerService.counter(id);
     }
 
     @PostMapping("/createJob")
     public ResponseEntity<String> createJob(@RequestBody JobPostingDTO jobPostingRequest) {
+        logger.info("Job Posting DTO Recieved: " + jobPostingRequest);
         System.out.println(jobPostingRequest);
         String message = interviewerService.createJob(jobPostingRequest);
         if (message.equals("Job created successfully")) {
@@ -82,6 +90,7 @@ public class InterviewerController {
 
     @GetMapping("/jobs/{id}")
     public ResponseEntity<List<JobInfoDTO>> getJobs(@PathVariable int id) {
+        logger.info("Given an interviewer-id request to fetch all the jobs recieved: " + id);
         List<JobInfoDTO> jobs = interviewerService.getJobs(id);
         if (!jobs.isEmpty()) {
             return ResponseEntity.ok(jobs);
@@ -92,6 +101,7 @@ public class InterviewerController {
 
     @GetMapping("/job/{id}")
     public ResponseEntity<Optional<JobForCandidateMicroserviceDTO>> getJob(@PathVariable int id) {
+        logger.info("Job For Candidate Microservice DTO recieved: " + id);
         Optional<JobForCandidateMicroserviceDTO> job = interviewerService.getJob(id);
         if (!job.isEmpty()) {
             return ResponseEntity.ok(job);
@@ -102,6 +112,7 @@ public class InterviewerController {
 
     @PostMapping("/job/enroll")
     public ResponseEntity<String> enrollInJob(@RequestBody JobEnrollDTO jobEnrollRequest) {
+        logger.info("Job Enroll DTO recieved: " + jobEnrollRequest);
         boolean status = interviewerService.enrollInJob(jobEnrollRequest);
         if (status) {
             return ResponseEntity.ok("Enrollment Successful");
@@ -112,6 +123,8 @@ public class InterviewerController {
 
     @GetMapping("/jobEnrollments/{jobId}")
     public ResponseEntity<List<JobEnrollmentInfoDTO>> getJobEnrollments(@PathVariable Long jobId) {
+        logger.info("Request to get job enrollments received: " + jobId);
+
         List<JobEnrollmentInfoDTO> jobs = interviewerService.getJobEnrollments(jobId);
         if (!jobs.isEmpty()) {
             return ResponseEntity.ok(jobs);
@@ -122,6 +135,8 @@ public class InterviewerController {
 
     @PostMapping("/job/updateTestScore")
     public ResponseEntity<String> updateTestScore(@RequestBody UpdateTestScoreDTO updateTestScoreRequest) {
+        logger.info("Update Test Score DTO received: " + updateTestScoreRequest);
+
         boolean status = interviewerService.updateTestScore(updateTestScoreRequest);
         if (status) {
             return ResponseEntity.ok("Test Score Updated Successful");
@@ -132,6 +147,8 @@ public class InterviewerController {
 
     @PostMapping("/code-sync-candidate-check/{candidateId}")
     public boolean candidateCheck(@PathVariable String candidateId, @RequestBody String roomId){
+        logger.info("Candidate Code-sync check request received");
+
         CandidateCheckDTO dto = new CandidateCheckDTO();
         dto.setCandidateName(candidateId);
         dto.setRoomId(roomId);
@@ -142,11 +159,14 @@ public class InterviewerController {
 
     @PostMapping("/code-sync-interviewer-check/{interviewerEmail}")
     public boolean interviewerCheck(@PathVariable String interviewerEmail){
+        logger.info("Interviewer Code-sync check request received");
+
         System.out.println(interviewerEmail);
         return interviewerService.interviewerCodeEditorCheck(interviewerEmail);
     }
     @GetMapping("/result/{enrollId}")
     public InterviewRecord results(@PathVariable int enrollId){
+        logger.info("Request to check results for a enrollment received: " + enrollId);
 
         return interviewerService.checkResults(enrollId);
     }
